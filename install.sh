5207cfff564762ee8c8db909d9b06f8f0448167f
carloid <carlosaugustogurgel2@gmail.com>
2026-09-02 22:28:21 -0400

    fix: xterm settings

--- install.sh
+++ install.sh
@@ -12,3 +12,6 @@ cp base16-default-dark-256.Xresources ~/
 # Append the source to the custom environment variables, if not present already
 BASHRC_APPEND="source ~/.env_vars.sh"
 grep -qxF "$BASHRC_APPEND" ~/.bashrc || echo "$BASHRC_APPEND" >> ~/.bashrc
+
+# Apply XTerm settings
+xrdb -merge ~/.Xresources
